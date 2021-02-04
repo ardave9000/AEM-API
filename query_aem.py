0ada33b6bec834bf066a7f9c753f93fabba71359
Adarsh Dave <adarsh.r.dave@MacBook-Pro.local>
2021-02-03 19:26:43 -0500

    Updates to query_aem temperature

--- query_aem.py
+++ query_aem.py
@@ -2,6 +2,7 @@ import argparse
 import os.path
 import json
 from ElectrolyteComposition import ElectrolyteComposition,AEM_API
+import pandas as pd
 
 def is_valid_file(parser, arg):
     if not os.path.exists(arg):
@@ -21,6 +22,8 @@ parser.add_argument("-f", dest="json_filename", required=True,
 parser.add_argument('-o', type=str, nargs=1,dest="output_filename",required=False,
 					help="Name of electrolyte in inventory",metavar="STR")
 
+parser.add_argument('-T', type=int, nargs=1,dest="temperature",required=False,
+					help="Temperature (C) for AEM model, if not supplied assume 25C",metavar="STR")
 #PROCESS
 from ElectrolyteComposition import ElectrolyteComposition
 
@@ -29,6 +32,10 @@ from ElectrolyteComposition import ElectrolyteComposition
 args = parser.parse_args()
 specs=json.load(args.json_filename)
 assert len(specs["salts"])>0, "Must supply salt in electrolyte to query AEM"
+
+if args.temperature:
+	assert args.temperature<=60 and args.temperature>=20,"Temperature selected must be between 20, 60"
+
 if specs["method"]=="by_mass_fraction_and_molality":
 	el=ElectrolyteComposition.by_mass_fraction_and_molality(solvents=specs["solvents"],salts=specs["salts"])
 	#name, valve, serial, solventDB_json, saltDB_json, date_created, density, viscosity
@@ -49,6 +56,14 @@ if args.output_filename:
 else:
 	out_fn="output.csv"
 
-aem.data[30].to_csv(out_fn)
+if args.temperature:
+	df=aem.data[args.temperature[0]]
+
+else:
+	df_concat=pd.concat(aem.data[20],aem.data[30])
+	by_row_index = df_concat.groupby(df_concat.index)
+	df = by_row_index.mean()
+
+df.to_csv(out_fn)
 
 print("Complete - results in {}".format(out_fn))
