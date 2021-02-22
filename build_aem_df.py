d21a47a30c9eb473e80ca9eefe0426a4add00eac
Adarsh Dave <adarsh.r.dave@MacBook-Pro.local>
2021-02-21 23:38:38 -0500

    updates

--- build_aem_df.py
+++ build_aem_df.py
@@ -9,7 +9,8 @@ import json
 
 solvent_disc=100 #uL
 mix_volume=1900
-components=["DMC_EC|50_50|LiPF6|2","DMC_EC|70_30|LiPF6|2","DMC_EC|70_30","DMC_EC|50_50"]
+densities_all={"DMC_EC|60_40|LiPF6|1.5":1.296,"DEC_EC|60_40|LiPF6|1.5":1.226,"DMC_EC|60_40":1.166,"DEC_EC|60_40":1.097}
+components=list(densities_all.keys())
 
 #MAKE CONFIG JSON
 if False:
@@ -27,20 +28,18 @@ if False:
 	config["domain_constraints"]=constraints
 	json.dump(config,open("df_config.json","w"))
 
-
 #QUERY AEM AT THAT DISCRETIZATION
 if True:
-	step=0.01
-	dim1=np.arange(0.30,0.50+step,step)
-	#dim1=[x/100.0 for x in [30,50]]
-	salt_m=2
+	dim1=[float(x) for x in '0.0-0.05-0.1-0.15-0.2-0.25-0.3-0.35-0.4-0.45-0.5-0.55-0.6-0.65-0.7-0.75-0.8-0.85-0.9-0.95-1.0'.split("-")]
+	salt_m=1.5
 	solvent="EC"
-	cosolvent="DMC"
+	cosolvent1="DMC"
+	cosolvent2="DEC"
 	salt="LiPF6"
 	out_fn="aem_run.csv"
 	dfs=[]
-	for solvent_mass_fraction in dim1:
-		el=ElectrolyteComposition.by_mass_fraction_and_molality(solvents={solvent:solvent_mass_fraction,cosolvent:1-solvent_mass_fraction},
+	for cosolvent_ratio in dim1:
+		el=ElectrolyteComposition.by_mass_fraction_and_molality(solvents={solvent:0.4,cosolvent1:cosolvent_ratio*0.6,cosolvent2:(1-cosolvent_ratio)*0.6},
 																salts={salt:salt_m})
 		aem=AEM_API(electrolyte=el)
 		aem.generate_cues()
@@ -51,13 +50,12 @@ if True:
 		aem30=aem.data[30]
 		aem20["T"]=[20]*len(aem20)
 		aem30["T"]=[30]*len(aem30)
-
 		curr_df=pd.concat([aem20,aem30])
 		# by_row_index = df_concat.groupby(df_concat.index)
 		# curr_df = by_row_index.mean()
 		#Add addt'l columns
-		curr_df["CID"]=[el.CompositionID]*len(curr_df)
-		curr_df["EC_mass_frac"]=[solvent_mass_fraction]*len(curr_df)
+		#curr_df["CID"]=[el.CompositionID]*len(curr_df)
+		curr_df["dim1"]=[cosolvent_ratio]*len(curr_df)
 		dfs.append(curr_df)
 		#print("Returned {} lines, {} columns from AEM".format(len(aem.data[30]),len(aem.data[30].columns)))
 
